f280883262129f39111816c85bc0f534df1b30ba
Shaaban12 <shaaban.n.mohammed@gmail.com>
2022-07-30 18:10:42 +0200

    created the checker function 	created next and previous slide function 	created Remove all active class function

--- js/main.js
+++ js/main.js
@@ -10,12 +10,96 @@ var prevBtn = document.getElementById('prev');
 nextBtn.onclick = nextSlide;
 prevBtn.onclick = prevSlide;
 
+
+
+
+// انشاء قائمه واعطائها ID
+// ul
+var paginationElement = document.createElement('ul');
+paginationElement.setAttribute('id', 'pagination-ul');
+
+
+// اضافه العناصرالى القائمه
+// li
+for(var i= 1; i <= slidersCount; i++) {
+
+    var paginationItem = document.createElement('li');
+    paginationItem.setAttribute('data-index', i);
+    paginationItem.appendChild(document.createTextNode(i));
+    // added the li to ul
+    paginationElement.appendChild(paginationItem);
+}
+document.getElementById('indicators').appendChild(paginationElement);
+
+
+// استدعاء القائمه
+var paginationCreatedUl = document.getElementById('pagination-ul');
+var paginationBullets = Array.from(document.querySelectorAll('#pagination-ul li'));
+
+// loop through all bullets
+paginationBullets.forEach(function (bulletClicked) {
+    bulletClicked.onclick = function () {
+        currentSlide = parseInt(this.getAttribute('data-index'));
+        theChecker();
+    }
+}); 
+
+// Trigger The Checker Function
+theChecker();
+
 // next slide function
 function nextSlide(){
-    console.log('next')
+    if (nextBtn.classList.contains('disabled')) {
+        return false;
+    }else {
+        currentSlide++;
+        theChecker()
+        console.log(currentSlide);
+    }
 }
 function prevSlide(){
-    console.log('prev')
+    if (prevBtn.classList.contains('disabled')) {
+        return false;
+    }else {
+    currentSlide--;
+    theChecker()
+    console.log(currentSlide);
+    }
+}
+
+// created the checker function 
+function theChecker() {
+    removeAllActive();
+    slideNumberElement.textContent = 'Slide #' + (currentSlide) + ' of ' + (slidersCount);
+    sliderImgs[currentSlide - 1].classList.add('active');
+    paginationCreatedUl.children[currentSlide - 1].classList.add('active');
+    
+    // check if current slide is the first
+    if (currentSlide == 1) {
+        prevBtn.classList.add('disabled');
+    }else {
+        prevBtn.classList.remove('disabled');
+    }
+    // check if current slide is the last
+    if (currentSlide == slidersCount) {
+        nextBtn.classList.add('disabled');
+    }else {
+        nextBtn.classList.remove('disabled');
+    }
+
+}
+
+
+// Remove all active class 
+function removeAllActive() {
+    // loop throuth imgs 
+    sliderImgs.forEach(function(img) {
+        img.classList.remove('active');
+    });
+    // loop throuth Bullets 
+    paginationBullets.forEach(function(bullet) {
+        bullet.classList.remove('active');
+    });
 }
 
 
